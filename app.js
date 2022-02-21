c466d217aaa70ff5c98563820f52221325d61e57
svadhis <imeian@gmail.com>
2022-02-21 22:31:58 +0100

    boilerplate

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const colors = ['blue', 'red', 'yellow', 'green', 'orange', 'emerald', 'lime', 'teal', 'cyan', 'indigo', 'violet', 'purple', 'fuchsia', 'pink', 'rose']
+
 let userInLobbyDiv = `
 <div class="user w-20 h-20 m-4 rounded-full border-4 text-xs border-white flex items-center justify-center text-white bg-blue-900">
     <span>USERNAME</span>
@@ -5,7 +7,7 @@ let userInLobbyDiv = `
 `
 
 let userInRoomDiv = `
-<div class="user w-32 h-32 m-6 rounded-full border-4 text-xl border-white flex items-center justify-center text-white bg-blue-900">
+<div class="user w-32 h-32 m-6 rounded-full border-4 text-xl border-white flex items-center justify-center text-white bg-blue-900 bg-">
     <span>USERNAME</span>
 </div>
 `
