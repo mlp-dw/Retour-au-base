05020481f047b71dc6ebfd20f6eb982f4809fc3f
svadhis <imeian@gmail.com>
2022-02-21 22:28:05 +0100

    boilerplate

---
+++ app.js
@@ -0,0 +1,12 @@
+let userInLobbyDiv = `
+<div class="user w-20 h-20 m-4 rounded-full border-4 text-xs border-white flex items-center justify-center text-white bg-blue-900">
+    <span>USERNAME</span>
+</div>
+`
+
+let userInRoomDiv = `
+<div class="user w-32 h-32 m-6 rounded-full border-4 text-xl border-white flex items-center justify-center text-white bg-blue-900">
+    <span>USERNAME</span>
+</div>
+`
+
